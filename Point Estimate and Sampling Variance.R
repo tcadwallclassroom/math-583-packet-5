e1b643a91c832b75e513748f1288e03a08548881
tcadwall <97193754+tcadwall@users.noreply.github.com>
2022-03-14 17:44:55 -0700

    Finished packet

---
+++ Point Estimate and Sampling Variance.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+population_size <- 259000000
+p <- 0.452
+sample_size <- 100
+population <- c(rep("approve",population_size * p), rep("not",population_size * (1-p)))
+sample <- sample(population, sample_size)
+sample_approve <- sum(sample == "approve")
+p_hat <- sample_approve / sample_size
+
+### For loop version
+
+simulations <- 1000
+p_hat_results <- numeric(simulations)
+for (i in 1:simulations) {
+  sample <- sample(population, sample_size)
+  sample_approve <- sum(sample == "approve")
+  p_hat <- sample_approve / sample_size
+  p_hat_results[i] <- p_hat
+}
+results <- data.frame(p_hat = p_hat_results)
+
+results %>% ggplot(aes(x = p_hat)) +
+  geom_histogram(color = "black", fill = "forestgreen", binwidth = 0.01) +
+  geom_vline(aes(xintercept = p), linetype = "dashed", color = "firebrick") +
+  labs(
+    title = "Distribution of Sampled Approval Rates",
+    subtitle = paste("Using", simulations, "simulated sample proportions with p = ", p),
+    x = "Sample proportion",
+    y = "Number of simulations"
+  ) +
+  scale_x_continuous(limits = c(0.00,1.00), breaks = seq(0.00,1.00,0.10))
+
+mean(results$p_hat)
+sd(results$p_hat)
